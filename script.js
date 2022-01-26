081b947e1ea6b483c48a1c2086ae75a153d6dade
Lauren Castro <laurcastro.l.c@gmail.com>
2022-01-26 13:46:53 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("I'm having a fun time in class. Took me a while to figure out how to space. Turns out, I just needed to space the quotation after my first name.")
+
+const age = 22;
+
+let firstName;
+ firstName = "Lauren ";
+
+ let lastName;
+ lastName = "Castro";
+
+alert("My name is " + firstName + lastName);
+alert("I am " + age);
